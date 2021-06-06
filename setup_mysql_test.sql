53288032cca2ce705267b7aaed5fefadb49a201b
chrisvanndy <chrisvannyc@gmail.com>
2021-06-06 18:37:10 +0000

    copy of hbnb_dev to hbhb_test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,15 @@
+-- Create DATABASE - hbnb_dev_db if does not exist
+-- Create USER - hbnb_dev (in localhost) if not exist
+-- USER password give as 'hbnb_dev_pwd
+-- GRANT USER ALL privileges on hbnb_dev_db
+-- GRANT USER SELEDT privileges on performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_test;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEGES;

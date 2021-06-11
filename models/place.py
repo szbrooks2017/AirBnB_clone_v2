f3f83fb1c9c7c987f45ba587cba19a3907396ced
chrisvanndy <chrisvannyc@gmail.com>
2021-06-11 15:41:10 +0000

    readded comma for syntax

--- models/place.py
+++ models/place.py
@@ -15,7 +15,7 @@ place_amenity = Table('place_amenity', metadata,
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False))
+                             primary_key=True, nullable=False),)
 
 
 class Place(BaseModel, Base):

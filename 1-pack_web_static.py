af328e5ae06586afdbcdea9baca8ecef537bab9e
chrisvanndy <chrisvannyc@gmail.com>
2021-06-11 20:13:17 +0000

    workshop task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Compressing a fabric file"""
+from fabric.api import local
+from datetime import datetime
+
+    def do_pack():
+        """ pack up out web_static directory """
+
+        now = datetime.now()
+
+        tarArchiveName = "web_static"+ now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        tarArchivePath = "versions/" + tarArchiveName
+
+        local("mkdir -p versions")
+
+        local("tar -czvf + tarArchivePath + " web_static")

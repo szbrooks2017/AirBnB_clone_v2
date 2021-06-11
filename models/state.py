3cd96edadbdbedddee34f17bf45786a9fc890604
chrisvanndy <chrisvannyc@gmail.com>
2021-06-11 15:37:08 +0000

    pep8

--- models/state.py
+++ models/state.py
@@ -5,19 +5,20 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state")
-    
+
     @property
     def cities(self):
         """getter for cities"""
         from models import storage
         get_cities = []
-        for k,v in storage.all(City).items():
+        for k, v in storage.all(City).items():
             if v.state_id == self.id:
                 get_cities.append(v)
         return get_cities

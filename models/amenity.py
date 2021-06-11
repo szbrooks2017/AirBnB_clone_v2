ef197916121184669d7e92e2478affb5614558f0
chrisvanndy <chrisvannyc@gmail.com>
2021-06-11 15:35:10 +0000

    pep8 for amnity

--- models/amenity.py
+++ models/amenity.py
@@ -5,12 +5,10 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
 
+
 class Amenity(BaseModel, Base):
     """ The Amenity Class """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
                                    backref="amenities")
-
-
-       

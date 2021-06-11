30be3cc256a50f1924b061801b917a85b2c44933
chrisvanndy <chrisvannyc@gmail.com>
2021-06-11 23:00:23 +0000

    tasks 2 and 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,6 +6,16 @@ import os.path
 
 env.hosts = ['18.208.222.5', '54.146.253.171']
 
+def do_deploy():
+    """ deploys dopack to archive"""
+    archive = do_pack()
+    if archive is None:
+        return False
+    
+    status = do_deploy(archive)
+
+    return status
+
 def do_deploy(archive_path):
     """deploy to web-server"""
     if not os.path.exists(archive_path):

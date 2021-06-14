fe68b5106905708397db7f050fc8eccb8c6a460a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-14 15:54:33 +0000

    adding task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ Compressing a fabric file"""
-from fabric.api import local, env, put, run
+
+from fabric.api import *
 from datetime import datetime
 import os.path
 
-env.hosts = ['18.208.222.5', '54.146.253.171']
+env.hosts = ['35.231.1.112', '54.167.91.234']
 
-def do_deploy():
+
+def deploy():
     """ deploys dopack to archive"""
     archive = do_pack()
     if archive is None:
@@ -18,33 +20,43 @@ def do_deploy():
 
 def do_deploy(archive_path):
     """deploy to web-server"""
+
     if not os.path.exists(archive_path):
         return False
 
     try:
         archiveName = archive_path[9:]
-        archiveNameWOEx = archive_path[:-4]
-
+        archiveNameWithoutExtention = archiveName[:-4]
         put(archive_path, '/tmp/' + archiveName)
-        run("mkdir -p /data/web_static/releases/" + archiveNameWOEx
-        run("tar -xzvf /tmp/ " +  archiveName + "-C" + "/data/web_static/releases/" archiveNameWOEx + " --strip-components=1")
-        run("rm -f /tmp/" + archiveName)
-        run("rm -f /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/" + archiveNameWOEx + " /data/web_static/current")
+        run("mkdir -p /data/web_static/releases/" +
+            archiveNameWithoutExtension)
+        # no space after /tmp/ add a space before -c
+        run('tar -xzvf /tmp/' + archiveName +
+            " -C /data/web_static/releases/" +
+            archiveNameWithoutExtension + " --strip-components=1")
+        run("rm -rf /tmp/" + archiveName)
+        run("rm -rf /data/web_static/current")
+        run("sudo ln -sf /data/web_static/releases/" +
+            archiveNameWithoutExtention + " /data/web_static/current")
 
         return True
     except:
         return False
 
+
 def do_pack():
-    """ pack up out web_static directory """
+    """ Pack up our web_static """
 
-    now = datetime.now()
+    try:
+        now = datetime.now()
 
-    tarArchiveName = "web_static_"+ now.strftime("%Y%m%d%H%M%S") + ".tgz"
-    tarArchivePath = "versions/" + tarArchiveName
+        tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        tarArchivePath = "versions/" + tarArchiveName
 
-    local("mkdir -p versions")
+        local("mkdir -p versions")
 
-    local("tar -czvf " + tarArchivePath + " web_static")
+        local("tar -czvf " + tarArchivePath + " web_static")
 
+        return tarArchivePath
+    except:
+        return None

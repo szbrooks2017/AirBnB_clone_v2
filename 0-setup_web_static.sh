08c69e70a45654fb5a3adbd36eedff9ab85396ad
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-14 14:28:46 +0000

    adding my task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,19 +10,16 @@ sudo service ngingx start
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 
-# may not need touch
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "Dummy Data" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "Pokemon" | sudo tee /data/web_static/releases/test/index.html
 
-ln -sfn /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -hR ubuntu:ubuntu /data
+# add sudo
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# add /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
 find="^\tlocation / {"
 replace="\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\tlocation / {"
-
 sudo sed -i "s@${find}@${replace}@" /etc/nginx/sites-available/default
 
 sudo service nginx restart
-
 exit 0

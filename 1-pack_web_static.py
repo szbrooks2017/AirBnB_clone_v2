12ba265f0050a549654bedaba774f6eff6cb03f6
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-14 14:33:06 +0000

    adding my task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,11 +8,13 @@ from datetime import datetime
 def do_pack():
     """ pack up out web_static directory """
 
+try:
     now = datetime.now()
-
-    tarArchiveName = "web_static" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+    # add _ after web static
+    tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
     tarArchivePath = "versions/" + tarArchiveName
-
-    local("mkdir -p versions")
-
-    local("tar -czvf" + tarArchivePath + " web_static")
+    local("sudo mkdir -p versions")
+    local("tar -czvf " + tarArchivePath + " web_static")
+    return tarArchivePath
+except:
+    return None

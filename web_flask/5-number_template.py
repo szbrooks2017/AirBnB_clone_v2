1ea8e26263034bea1d609334f0cc79ffe6b7247c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-21 16:07:08 +0000

    adding {{ n }}

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -30,6 +30,7 @@ def p_text(text="is cool"):
 def n_number(n):
     return "{} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def n_template(n):
     return render_template('5-number.html', n=n)

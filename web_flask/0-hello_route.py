6cab1145fb1e58bd8283f0bedbdf731da58a9403
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-21 03:46:45 +0000

    adding task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Script that starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run()

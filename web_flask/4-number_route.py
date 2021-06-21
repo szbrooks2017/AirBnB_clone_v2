bc23f300beb758852d51ad9cb86ad1ce95ee5e23
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-21 15:07:18 +0000

    adding a number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Script that starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def p_text(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_number(n):
+    return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')

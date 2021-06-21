b0419c91107b39e6c6624ca7ac97469a73d4d9ca
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-21 15:47:02 +0000

    adding task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Script that starts a flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def p_text(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_number(n):
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def n_template(n):
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')

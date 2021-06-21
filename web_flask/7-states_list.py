a57d3896760f9a1e1f219b47d6fcee8b46cba4e3
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-21 18:50:29 +0000

    adding task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Script that starts a flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list/")
+def s_list():
+    s = storage.all(State).values()
+    return render_template('7-states_list.html', s=s)
+
+@app.teardown_appcontext
+def teardown(error):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)

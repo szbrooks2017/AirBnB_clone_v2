5c3e05deb9c903ec8bbbbcc4c1d5de3020f80bf1
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-21 14:54:29 +0000

    without parentheses

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,23 +4,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
-
 @app.route('/hbnb', strict_slashes=False)
 def display():
     return "HBNB"
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return "C {}".format(text.replace('_', ' '))
 
 @app.route('/python/')
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def p_text(text="is cool"):
     return "Python {}".format(text.replace('_', ' '))
 

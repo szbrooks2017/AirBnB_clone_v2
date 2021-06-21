8506ac0503ca21f3fff32f82c7c2d4f7b23a1bd4
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-21 06:10:36 +0000

    adding default text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script that starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def display():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/(<text>)')
+def p_text(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')

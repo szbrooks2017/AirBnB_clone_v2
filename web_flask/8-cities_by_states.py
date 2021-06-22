4edc8b42e4bcf2fb0df024e67f185373c440e6eb
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-22 00:45:21 +0000

    fixing variables

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,15 +8,11 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states_list/", strict_slashes=False)
-def s_list():
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
-
 @app.route("/cities_by_states/", strict_slashes=False)
 def c_list():
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown(error):

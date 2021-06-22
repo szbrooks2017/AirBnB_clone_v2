d9166ee07646ad6385197245a0b92acbf0d16431
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-22 03:00:43 +0000

    can get states

---
+++ web_flask/9-states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Script that starts a flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states/", strict_slashes=False)
+def state_list():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+@app.route("/state/<id>", strict_slashes=False)
+def state_by_id(id):
+    states = storage.all(State).values()
+    for state in states:
+        if id == state.id:
+            return render_template('9-states.html', states=states, found=True)
+    return render_template('9-states.html')
+
+@app.teardown_appcontext
+def teardown(error):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
